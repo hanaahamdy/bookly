5e039d19b7767afd0c715d975a08c486ec6ce6a8
hanaahamdy <hanaahamdy514@gmail.com>
2023-05-14 19:31:24 +0300

    add test file to testing branch:

---
+++ lib/testing_branch.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+class TestingBranch extends StatelessWidget {
+  const TestingBranch({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(body: Container(height: 100,width: 160,
+      child:const Text("testing branch"),));
+  }
+}
